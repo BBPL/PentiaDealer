18a3cd5a601439e5775bfcbd10599cc6297dc24d
Uli Weltersbach <uw@pentia.dk>
2018-08-31 10:48:00 +0200

    Reducing code duplication - step 2
    "baseQuery" extracted for "SearchCustomers".

--- PentiaDealer/Controllers/SearchController.cs
+++ PentiaDealer/Controllers/SearchController.cs
@@ -18,62 +18,53 @@ namespace PentiaDealer.Controllers
 
         public JsonResult SearchCustomers(string SearchSpec, string SearchValue)
         {
+            var baseQuery = from cp in context.CarPurchases
+                            from c in context.Cars
+                            from cus in context.Customers
+                            from sp in context.SalesPeople
+                            where cp.CustomerId == cus.CustomerId
+                            where cp.CarId == c.CarId
+                            where cp.SalesPersonId == sp.SalesPersonId
+                            select new { cus, c, sp, cp };
+
             switch (SearchSpec)
             {
                 case "CustomerFirstName":
-                    var res = from cp in context.CarPurchases
-                              from c in context.Cars
-                              from cus in context.Customers
-                              from sp in context.SalesPeople
-                              where cp.CustomerId == cus.CustomerId
-                              where cp.CarId == c.CarId
-                              where cp.SalesPersonId == sp.SalesPersonId
-                              where cus.Name.Contains(SearchValue)
+                    var res = from x in baseQuery
+                              where x.cus.Name.Contains(SearchValue)
                               select new Result
                               {
-                                  Customers = cus,
-                                  Cars = c,
-                                  SalesPerson = sp,
-                                  BuyDate = cp.OrderDate,
-                                  Price = cp.PricePaid
+                                  Customers = x.cus,
+                                  Cars = x.c,
+                                  SalesPerson = x.sp,
+                                  BuyDate = x.cp.OrderDate,
+                                  Price = x.cp.PricePaid
                               };
                     return Json(res);
 
                 case "CustomerLastName":
-                    var res2 = from cp in context.CarPurchases
-                               from c in context.Cars
-                               from cus in context.Customers
-                               from sp in context.SalesPeople
-                               where cp.CustomerId == cus.CustomerId
-                               where cp.CarId == c.CarId
-                               where cp.SalesPersonId == sp.SalesPersonId
-                               where cus.Surname.Contains(SearchValue)
+                    var res2 = from x in baseQuery
+                               where x.cus.Surname.Contains(SearchValue)
                                select new Result
                                {
-                                   Customers = cus,
-                                   Cars = c,
-                                   SalesPerson = sp,
-                                   BuyDate = cp.OrderDate,
-                                   Price = cp.PricePaid
+                                   Customers = x.cus,
+                                   Cars = x.c,
+                                   SalesPerson = x.sp,
+                                   BuyDate = x.cp.OrderDate,
+                                   Price = x.cp.PricePaid
                                };
 
                     return Json(res2);
                 case "CustomerAddress":
-                    var res3 = from cp in context.CarPurchases
-                               from c in context.Cars
-                               from cus in context.Customers
-                               from sp in context.SalesPeople
-                               where cp.CustomerId == cus.CustomerId
-                               where cp.CarId == c.CarId
-                               where cp.SalesPersonId == sp.SalesPersonId
-                               where cus.Address.Contains(SearchValue)
+                    var res3 = from x in baseQuery
+                               where x.cus.Address.Contains(SearchValue)
                                select new Result
                                {
-                                   Customers = cus,
-                                   Cars = c,
-                                   SalesPerson = sp,
-                                   BuyDate = cp.OrderDate,
-                                   Price = cp.PricePaid
+                                   Customers = x.cus,
+                                   Cars = x.c,
+                                   SalesPerson = x.sp,
+                                   BuyDate = x.cp.OrderDate,
+                                   Price = x.cp.PricePaid
                                };
 
                     return Json(res3);
